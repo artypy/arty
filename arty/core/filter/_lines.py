d9f7ecbeb77f49a1850b469095fae7d543484919
JIeruOH <136506317+JIeruOH@users.noreply.github.com>
2024-11-23 05:03:49 +0300

    Update _lines.py
    rewrite drawing from pygame to cv2

--- arty/core/filter/_lines.py
+++ arty/core/filter/_lines.py
@@ -1,15 +1,16 @@
 import numpy as np
-import pygame
 from math import radians, cos, sin
+import cv2
 
 
-def lines(image: np.ndarray, angles: np.ndarray, edges: np.ndarray) -> np.ndarray:
+def lines(image: np.ndarray, angles: np.ndarray, edges: np.ndarray, thickness: int = 3,
+          max_length: int = 10) -> np.ndarray:
     priority = [(edges[y, x], (x, y)) for y in range(len(edges)) for x in range(len(edges[y]))]
-    h, w, c = image.shape
-    new = pygame.Surface((w, h))
-    for _, (x, y) in priority[::-1]:
-        if new.get_at((x, y)) != (0, 0, 0, 255): continue
+    priority.sort(reverse=True)
+    new = np.zeros_like(image)
+    for _, (x, y) in priority:
+        if new[y, x].max() != 0: continue
         angle = radians(angles[y, x])
-        dy, dx = cos(angle) * 5, sin(angle) * 5
-        pygame.draw.line(new, image[y, x], (x - dx, y - dy), (x + dx, y + dy), 3)
-    return pygame.surfarray.pixels3d(new).transpose(1, 0, 2)
+        dy, dx = int(cos(angle) * max_length / 2), int(sin(angle) * max_length / 2)
+        cv2.line(new, (x - dx, y - dy), (x + dx, y + dy), image[y, x].tolist(), thickness)
+    return new
