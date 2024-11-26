f695c3ae4c3364a4f7a70e4e7dd90e23bc4fb0a5
Bulat Akhmatov <ahmatovbulat@gmail.com>
2024-11-27 01:20:12 +0300

    feat: new version of library

--- setup_arty.py
+++ setup_arty.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="artypy",
-    version="0.8.0",
+    version="0.10.0",
     author="artypy team",
     author_email="ahmatovbulat@gmail.com",
     description="Lightweight library for artistic filters",
@@ -19,12 +19,10 @@ setup(
     install_requires=[
         "numpy",
         "matplotlib",
-        "scikit-image",
-        "scikit-learn",
         "tqdm",
         "opencv-python",
         "progressbar",
-        "pygame",
+        "scipy"
     ],
     extras_require={
         "cuda": ["torch"],
