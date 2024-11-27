3d926b0ca6df574e9bc98c31797e59012aa5deb9
JIeruOH <136506317+JIeruOH@users.noreply.github.com>
2024-11-27 06:10:09 +0300

    Update __init__.py

--- arty/nn/__init__.py
+++ arty/nn/__init__.py
@@ -1,6 +1,6 @@
 # check if torch is installed, if yes, from . import cnn, if not, do not import cnn
 try:
     import torch
-    from .cnn import CNN
+    from ._cnn import CNN
 except ImportError:
     pass
