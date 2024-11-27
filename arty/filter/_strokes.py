b3e39d9b1765b497433e698ae525db170c794dac
JIeruOH <136506317+JIeruOH@users.noreply.github.com>
2024-11-27 09:41:28 +0300

    Update _strokes.py

--- arty/filter/_strokes.py
+++ arty/filter/_strokes.py
@@ -14,16 +14,16 @@ class Strokes:
         if angles is None:
             angles = GradientTruth().predict(image)
         if points is None:
-            points = Points(7, 1, 0).process(image)
+            points = Points().process(image)
         pts = points
         gx = np.concatenate((np.abs(image[:, 1:] - image[:, :-1]).sum(axis=2), np.zeros((h, 1), dtype=int)), axis=1)
         gy = np.concatenate((np.abs(image[1:, :] - image[:-1, :]).sum(axis=2), np.zeros((1, w), dtype=int)), axis=0)
         grad = np.maximum(gx, gy) / 765
-        surf = np.ones_like(image) * 255
+        surf = np.ones_like(image) * 200
         image = 1 - image.mean(axis=2) / 255
         h, w = pts.shape
 
-        angles[grad < 0.05] = np.random.random(angles.shape)[grad < 0.05] * 180
+        angles[grad < 0.01] = np.random.random(angles.shape)[grad < 0.01] * 180
         angles = np.radians(angles)
         X, Y = np.cos(angles), np.sin(angles)
         for y in range(h):
@@ -31,6 +31,6 @@ class Strokes:
                 if pts[y, x]: continue
                 ln = image[y, x] * self.mx_length / 2
                 dx, dy = X[y, x], Y[y, x]
-                cv2.line(surf, (int(x + ln * dy), int(y + ln * dx)), (int(x - ln * dy), int(y - ln * dx)), (50, 50, 50),
+                cv2.line(surf, (int(x + ln * dy), int(y + ln * dx)), (int(x - ln * dy), int(y - ln * dx)), (75, 75, 75),
                          1)
         return surf
